e3290b70b1a3f936241d46c34007ff113013e105
edroteVilly <edrote.villy@gmail.com>
2025-05-06 17:23:32 +0800

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
     <title>Product List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body style="background-color: #f3e8ff;">
     <div class="container py-5">
         <h1 class="text-center mb-4">📦 Product Inventory</h1>
 
